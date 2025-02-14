d1e5a985c4ec6d471428e5176464851b5f8b5841
Serhiy Malysh <166062683+finconsultsm@users.noreply.github.com>
2025-02-14 07:35:54 +0100

    2

--- my-ts-project/src/generics/3.ts
+++ my-ts-project/src/generics/3.ts
@@ -1,3 +1,4 @@
-function merge<T, U>(objA: T, objB: U): T & U {
-  return Object.assign({}, objA, objB);
+function merge<T extends Record<string, any>, U extends Record<string, any>>(objA: T, objB: U): T & U {
+    return Object.assign({}, objA, objB);
 }
+
